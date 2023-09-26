4ff1dd6d4e786d698bb56cfbdde3ed004dc55da6
guri-sachin <107688069+guri-sachin@users.noreply.github.com>
2023-09-27 02:01:43 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import logo from './logo.svg';
+import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+
+import Home from './Component/Home';
+import Lall from './Component/Lall';
+import Header from './Component/Header';
+import Detail from './Component/Detail';
+
+function App() {
+  return (
+    <div className="App">
+     <Router>
+      <Routes>
+        
+        
+       
+      <Route exact path="/Lall" element={<Lall />}></Route>
+         <Route exact path="/" element={<Home />}></Route>
+         <Route exact path="/Detail" element={<Detail />}></Route>
+     
+    
+      </Routes>
+     </Router>
+    </div>
+  );
+}
+
+export default App;
